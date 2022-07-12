9ea4af9d8356a047a62280b4a7f3c10b67d7601d
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-07-12 20:31:33 +0200

    Coordinates

--- cursor.hpp
+++ cursor.hpp
@@ -29,30 +29,42 @@ public:
 
     void moveUp(int n) {
         std::cout << CSI << n << CUU_;
+        this->y -= n;
     }
     void moveDown(int n) {
         std::cout << CSI << n << CUD_;
+        this->y += n;
     }
     void moveForward(int n) {
         std::cout << CSI << n << CUF_;
+        this->x += n;
     }
     void moveBackward(int n) {
         std::cout << CSI << n << CUB_;
+        this->x -= n;
     }
+
     void nextLine(int n) {
         std::cout << CSI << n << CNL_;
+        this->y += n;
     }
     void previousLine(int n) {
         std::cout << CSI << n << CPL_;
+        this->y -= n;
     }
-    void horizontalAbsolute(int n) {
-        std::cout << CSI << n << CHA_;
+
+    void horizontalAbsolute(int x) {
+        std::cout << CSI << x << CHA_;
+        this->x = x;
     }
-    void verticalAbsolute(int n) {
-        std::cout << CSI << n << VPA_;
+    void verticalAbsolute(int y) {
+        std::cout << CSI << y << VPA_;
+        this->y = y;
     }
     void position(int x, int y) {
         std::cout << CSI << y << ';' << x << CUP_;
+        this->x = x;
+        this->y = y;
     }
 
     void savePosition() {
